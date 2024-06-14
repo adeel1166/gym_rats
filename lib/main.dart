597c0eb8b7abf078819b45ff6549d856c2f9aa67
Adeel Akhtar <adeeljatt1116@gmail.com>
2024-06-14 01:27:49 -0700

    Map Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:gym_rats/Model/UserInfoController.dart';
 import 'package:gym_rats/View/Intro/ExploreScreen.dart';
 
-void main(){
-Get.put(UserInfoController());
+Future<void> main() async {
+ await GetStorage.init();
+ Get.put(UserInfoController());
   runApp(MyApp());
   }
 
